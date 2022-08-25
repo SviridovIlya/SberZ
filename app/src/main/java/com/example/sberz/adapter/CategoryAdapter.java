efebddb9ce034ee426ea87ba5f848e501181db7d
SviridovIlya <sviridovila548@gmail.com>
2022-08-25 09:53:39 +0300

    SberZ

---
+++ app/src/main/java/com/example/sberz/adapter/CategoryAdapter.java
@@ -0,0 +1,54 @@
+package com.example.sberz.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.sberz.model.Category;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
+
+    Context context;
+    List<Category> categories;
+
+    public CategoryAdapter( Context context, List<Category>categories) {
+
+        this.context = context;
+        this.categories = categories;
+
+    }
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+
+    }
+
+    public static final class CategoryViewHolder extends RecyclerView.ViewHolder {
+
+
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+        }
+
+    }
+
+}
